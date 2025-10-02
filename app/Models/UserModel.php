53ad11239ef6847eecf3a0dd54871effed27234a
RiaSafitrii <ria.safitri2606@gmail.com>
2025-10-02 19:12:51 +0700

    Praktikum Pemrograman Web Lanjut

---
+++ app/Models/UserModel.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'user';
+    protected $guarded = ['id'];
+    
+    public function user() {
+        return $this->belongsTo(Kelas::class, 'kelas_id');
+    }
+}
